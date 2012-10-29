c2dd6e59addad0aa86c101ef5a6c15b06398588a
Jeff <guionj@gmail.com>
2012-10-29 14:48:44 -0400

    Moved generate train logic to JsonData class

--- src/main/java/softwaredev/purpleparrots/JsonData.java
+++ src/main/java/softwaredev/purpleparrots/JsonData.java
@@ -2,6 +2,9 @@ package softwaredev.purpleparrots;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -10,14 +13,42 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JsonData {
 	
-	public static JsonNode getTData(String line) throws JsonParseException, JsonMappingException, IOException{
+	public static JsonNode getTData(Line line) throws JsonParseException, JsonMappingException, IOException{
 		ObjectMapper mapper = new ObjectMapper();
-		URL orangeLineUrl = new URL("http://developer.mbta.com/lib/rthr/"+line+".json");
-		JsonNode rootNode = mapper.readValue(orangeLineUrl, JsonNode.class);
+		String color = line.getName();
+		URL url = new URL("http://developer.mbta.com/lib/rthr/"+color+".json");
+				
+		JsonNode rootNode = mapper.readValue(url, JsonNode.class);
 		
 		JsonNode tripList = rootNode.path("TripList");
 		JsonNode trips = tripList.path("Trips");
 		return trips;
+	}
+	
+	public static List<Train> getTrains(Line line) throws JsonParseException, JsonMappingException, IOException{
+		List<Train> trains = new ArrayList();
+		JsonNode trips_list = getTData(line);
+		Iterator<JsonNode> trips = trips_list.iterator();
+		while(trips.hasNext()){
+			JsonNode trip = trips.next();
+			String id = trip.path("TripID").asText();
+			String destination = trip.path("Destination").asText();
+			Iterator<JsonNode> predictions_iterator = trip.path("Predictions").iterator();
+			List<Prediction> predictions = new ArrayList();
+			while(predictions_iterator.hasNext()){
+				JsonNode prediction = predictions_iterator.next();
+				String stopId = prediction.path("StopID").asText();
+				String stop = prediction.path("Stop").asText();
+				int seconds = prediction.path("Seconds").asInt();
+
+				Prediction p = new Prediction(stopId, stop, seconds);
+				predictions.add(p);
+			}
+
+			Train train = new Train(id, line.getName(), destination, predictions);
+			trains.add(train);
+		}
+		return trains;
 	}
 
 }
--- src/main/java/softwaredev/purpleparrots/MyMbta.java
+++ src/main/java/softwaredev/purpleparrots/MyMbta.java
@@ -11,39 +11,19 @@ import com.fasterxml.jackson.databind.JsonNode;
 
 public class MyMbta {
 	
-	public static List<Train> getTrains() throws JsonParseException, JsonMappingException, IOException{
-		List<Train> trains = new ArrayList();
-		String[] lines = {"orange", "red", "blue"};
-		for(int i=0; i<3; i++){
-			
-			JsonNode trips_list = JsonData.getTData(lines[i]);
-			Iterator<JsonNode> trips = trips_list.iterator();
-			while(trips.hasNext()){
-				JsonNode trip = trips.next();
-				String id = trip.path("TripID").asText();
-				String destination = trip.path("Destination").asText();
-				Iterator<JsonNode> predictions_iterator = trip.path("Predictions").iterator();
-				List<Prediction> predictions = new ArrayList();
-				while(predictions_iterator.hasNext()){
-					JsonNode prediction = predictions_iterator.next();
-					String stopId = prediction.path("StopID").asText();
-					String stop = prediction.path("Stop").asText();
-					int seconds = prediction.path("Seconds").asInt();
-				
-					Prediction p = new Prediction(stopId, stop, seconds);
-					predictions.add(p);
-				}
-			
-				Train train = new Train(id, lines[i], destination, predictions);
-				trains.add(train);
-			}
-		}
-		return trains;
-	}
 	
 	public static void getCurrentLocationOfAllTrains() throws JsonParseException, JsonMappingException, IOException{
-		List<Train> trains = getTrains();
-		System.out.println(trains);
+		ArrayList<Line> lines = new ArrayList();
+		ArrayList<String> stations = new ArrayList();
+		Line orange = new Line("orange", stations);
+		Line red = new Line("red", stations);
+		Line blue = new Line("blue", stations);
+		List<Train> orange_trains = JsonData.getTrains(orange);
+		List<Train> red_trains = JsonData.getTrains(red);
+		List<Train> blue_trains = JsonData.getTrains(blue);
+		System.out.println(orange_trains);
+		System.out.println(red_trains);
+		System.out.println(blue_trains);
 	}
 	
 	
