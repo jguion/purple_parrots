f05686cf107fd4c9402b402763c7541e101e3e9a
Nicholas Labich <labichn@gmail.com>
2012-11-11 16:22:11 -0500

    Removed JDK 1.7 specific packages.

--- src/main/java/softwaredev/purpleparrots/JsonData.java
+++ src/main/java/softwaredev/purpleparrots/JsonData.java
@@ -1,11 +1,12 @@
 package softwaredev.purpleparrots;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.IOException;
 import java.io.File;
+import java.io.FileInputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -90,7 +91,12 @@ public class JsonData {
         String re = "";
         try {
             if (file != null && file.isFile()) {
-                re = new String(Files.readAllBytes(Paths.get(file.toURI())));
+                BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+                String tmp = "";
+                while ((tmp = br.readLine()) != null) {
+                    re += "\n" + tmp;
+                }
+                br.close();
             }
         } catch (Exception e) {
             log(e);
