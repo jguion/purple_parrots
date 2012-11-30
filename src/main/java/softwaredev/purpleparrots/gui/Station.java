f2a123ba1ac973b344f9184e5f9c50da1c35feb6
Nicholas Labich <labichn@gmail.com>
2012-11-29 19:52:08 -0500

    Added incoming trains to station tool tip.

--- src/main/java/softwaredev/purpleparrots/gui/Station.java
+++ src/main/java/softwaredev/purpleparrots/gui/Station.java
@@ -4,11 +4,17 @@ import java.awt.Color;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JDialog;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
+
+import softwaredev.purpleparrots.Line;
+import softwaredev.purpleparrots.MyMbta;
+import softwaredev.purpleparrots.Train;
 
 public class Station extends JPanel implements MouseListener{
 	private String name;
@@ -59,8 +65,77 @@ public class Station extends JPanel implements MouseListener{
 		}
 	}
 
+    /**
+     * Gets a filtered list of trains--those which currently have predictions for the given stop name.
+     * @param trains  the list of trains to filter
+     * @param name    the name of the stop which the trains must have in their predictions list
+     * @return        the list of trains which include predictions toward the given stop name
+     * @author        labichn
+     */
+    private List<Train> getTrainsForStop(List<Train> trains, String name) {
+        List<Train> re = new ArrayList<Train>();
+        if (name != null && trains != null) {
+            Train tmp = null;
+            for (int i=0; i<trains.size(); i++) {
+                tmp = trains.get(i);
+                if (tmp.hasPredFor(name)) re.add(tmp);
+            }
+            re = sort(re, name);
+        }
+        return re;
+    }
+
+    /**
+     * Slowly sorts the given list of trains. Calls to getPredFor are safe because
+     * we've already filtered on the existance of a prediction with the given stop name.
+     * @param trains  the trains to sort
+     * @return        the trains, sorted from earliest arrival to latest to the stop
+     *                denoted by the given stop name.
+     * @author        labichn
+     */
+    private List<Train> sort(List<Train> trains, String name) {
+        List<Train> re = new ArrayList<Train>();
+        Train lowest, test;
+        while (re.size()<trains.size()) {
+            lowest = null;
+            test = null;
+            for (int i=0; i<trains.size(); i++) {
+                test = trains.get(i);
+                if (!re.contains(test)) {
+                    if (lowest == null
+                     || test.getPredFor(name).seconds < lowest.getPredFor(name).seconds) {
+                        lowest = test;
+                    }
+                }
+            }
+            re.add(lowest);
+        }
+        return re;
+    }
+    
+    /**
+     * Gets a string representation of the predictions for this stop from the given list
+     * of trains.
+     * @param trains  the trains to get predictions out of
+     * @return        the string message for the tooltip.
+     * @autho 	      labichn
+     */
+    private String getTextForTrains(List<Train> trains) {
+    	String re = "";
+    	if (trains != null && trains.size() > 0) {
+    		re = "<html>";
+    		for (Train train : trains) {
+    			re += "Train " + train.getId() + " is " + String.valueOf(train.getPredFor(this.name).seconds) +
+    					" seconds away.<br>";
+    		}
+    		re = re.substring(0, re.length()-4) + "</html>";
+    	}
+    	return re;
+    }
+
 	public void mouseEntered(MouseEvent arg0) {
-		// TODO Auto-generated method stub
+		List<Train> trains = getTrainsForStop(MyMbta.getTrains(new Line(this.line)), this.name);
+		this.setToolTipText(getTextForTrains(trains));
 	}
 
 	public void mouseExited(MouseEvent arg0) {
